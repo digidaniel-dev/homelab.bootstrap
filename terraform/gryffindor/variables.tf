e8182668f56a748377b7266b487cb31760b560c5
Daniel Vollbro <wollbro90@gmail.com>
2025-08-07 20:19:58 +0200

    Added terraform configurations

---
+++ terraform/gryffindor/variables.tf
@@ -0,0 +1,53 @@
+// All providers
+
+variable "endpoint" {
+  type = string
+}
+
+// Proxmox ACME Plugin
+
+variable "cloudflare_account_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "cloudflare_api_token" {
+  type      = string
+  sensitive = true
+}
+
+// Proxmox ACME Account
+
+variable "email" {
+  type = string
+}
+
+// Networking
+
+variable "dns_servers" {
+  type      = list(string)
+  sensitive = true
+}
+
+// SSH provider
+
+variable "password" {
+  type      = string
+  sensitive = true
+}
+
+variable "ssh_user" {
+  type = string
+}
+
+variable "ssh_password" {
+  type      = string
+  sensitive = true
+}
+
+// API Provider
+
+variable "api_token" {
+  type      = string
+  sensitive = true
+}
