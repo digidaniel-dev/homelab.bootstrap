c2718db0f07caa4e170ad04f0276ce0a88322cbf
Daniel Vollbro <wollbro90@gmail.com>
2025-08-07 22:44:28 +0200

    migrated bootstrap terraform configuration

--- terraform/modules/proxmox-node/no-subscription-repository.tf
+++ terraform/modules/proxmox-node/no-subscription-repository.tf
@@ -1,13 +1,7 @@
-resource "proxmox_virtual_environment_apt_standard_repository" "no-sub-standard-repo" {
-  provider = proxmox.api
-  handle   = "no-subscription"
-  node     = var.node_name
-}
-
 resource "proxmox_virtual_environment_apt_repository" "no-sub-repo" {
   provider  = proxmox.api
   enabled   = true
-  file_path = proxmox_virtual_environment_apt_standard_repository.no-sub-standard-repo.file_path
-  index     = proxmox_virtual_environment_apt_standard_repository.no-sub-standard-repo.index
-  node      = proxmox_virtual_environment_apt_standard_repository.no-sub-standard-repo.node
+  file_path = "/etc/apt/sources.list.d/proxmox.sources"
+  index     = 0
+  node      = var.node_name
 }
--- terraform/modules/proxmox-node/provider.tf
+++ terraform/modules/proxmox-node/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     proxmox = {
       source  = "bpg/proxmox"
-      version = "~> 0.73.1"
+      version = "~> 0.81.0"
       configuration_aliases = [
         proxmox.api,
         proxmox.ssh
